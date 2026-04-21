ccabd97a27cf1e5346191a9f4bfa8eec7415ea6f
Preet <pritimotghare497@gmail.com>
2026-04-21 16:17:04 +0530

    Initial commit - Student Management System

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div>
+      <h1>Student Management System</h1>
+
+      <h2>Add Student</h2>
+
+      <input type="text" placeholder="Enter Name" />
+      <br /><br />
+
+      <input type="text" placeholder="Enter Class" />
+      <br /><br />
+
+      <button>Add Student</button>
+
+      <hr />
+
+      <h2>Student List</h2>
+      <p>No students added yet</p>
+    </div>
+  );
+}
+
+export default App;
