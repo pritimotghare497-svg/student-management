cb3bb9a398561ea5c3b1acc867c5e5ee4f4502bd
Preet <pritimotghare497@gmail.com>
2026-04-22 16:51:52 +0530

    Added Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [name, setName] = useState("");
-  const [students, setStudents] = useState([]);
-
-  const addStudent = () => {
-    if (name.trim() === "") return;
-
-    setStudents([...students, name]);
-    setName("");
-  };
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Student Management System</h1>
-
-      <h2>Add Student</h2>
-
-      <input
-        type="text"
-        placeholder="Enter Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <button onClick={addStudent}>Add Student</button>
-
-      <hr />
-
-      <h2>Student List</h2>
-
-      {students.length === 0 ? (
-        <p>No students added yet</p>
-      ) : (
-        <ul>
-          {students.map((student, index) => (
-            <li key={index}>{student}</li>
-          ))}
-        </ul>
-      )}
+    <div>
+      <Navbar />
+      <h1>Job Portal Project 🚀</h1>
     </div>
   );
 }
 
 export default App;
-
